354642d4f4a49bdc15df059fbe671a240478660d
Ankit <iankitraj18@gmail.com>
2026-03-27 02:59:37 +0530

    added cors setup in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 // Require all the routes here
 const authRouter = require("./routes/auth.routes");
@@ -8,6 +9,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 /* Using all the routes here */
 app.use("/api/auth", authRouter)
